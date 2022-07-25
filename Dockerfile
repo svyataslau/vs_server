6e4c8a06ef3400c6e041dda9083660b01fcedb0b
sbely <sbely@exadel.com>
2022-07-25 22:46:32 +0300

    feature: implemented flyway migrations & refactor db(add reasons table)

--- Dockerfile
+++ Dockerfile
@@ -2,4 +2,3 @@ FROM postgres
 ENV POSTGRES_USER someuser
 ENV POSTGRES_PASSWORD somepassword
 ENV POSTGRES_DB somedb
-COPY db.sql /docker-entrypoint-initdb.d/
---
+++ src/main/resources/db/migration/V1__initTables.sql
@@ -0,0 +1,44 @@
+CREATE TABLE user_profile (
+                              id SERIAL,
+                              nickname VARCHAR(128) UNIQUE,
+                              email VARCHAR(128) UNIQUE,
+                              PRIMARY KEY (id)
+);
+
+CREATE TABLE promise (
+                         id SERIAL,
+                         title VARCHAR(128),
+                         PRIMARY KEY (id)
+);
+
+CREATE TABLE user_challenge (
+                                id SERIAL,
+                                user_id INTEGER REFERENCES  user_profile(id) ON DELETE  CASCADE,
+                                promise_id INTEGER REFERENCES  promise(id) ON DELETE  CASCADE,
+                                description VARCHAR(128),
+                                start_date TIMESTAMP NOT NULL,
+                                days_number INTEGER NOT NULL,
+                                PRIMARY KEY (id)
+);
+
+CREATE TABLE reason (
+                        id SERIAL,
+                        user_challenge_id INTEGER REFERENCES  user_challenge(id) ON DELETE  CASCADE,
+                        description VARCHAR(128),
+                        PRIMARY KEY (id)
+);
+
+INSERT INTO user_profile (nickname, email) VALUES ('nikky34','some432@gmail.com');
+INSERT INTO user_profile (nickname, email) VALUES ('mortyRick','letsgo763@gmail.com');
+INSERT INTO user_profile (nickname, email) VALUES ('locky89','manhello543@gmail.com');
+INSERT INTO user_profile (nickname, email) VALUES ('heyheyhey666','dude345@gmail.com');
+
+INSERT INTO promise (title) VALUES ('Stop addiction');
+INSERT INTO promise (title) VALUES ('Get a new ability');
+INSERT INTO promise (title) VALUES ('Learn something new');
+INSERT INTO promise (title) VALUES ('Lose weight');
+
+INSERT INTO user_challenge (user_id, promise_id, description, start_date, days_number) VALUES (1,3,'I want to learn java & spring', '2019-02-15 10:32:02', 30);
+INSERT INTO user_challenge (user_id, promise_id, description, start_date, days_number) VALUES (2,4,'I have to lose 20 kg', '2019-02-15 10:54:02', 60);
+INSERT INTO user_challenge (user_id, promise_id, description, start_date, days_number) VALUES (3,3,'I want to read a book about front-end 50 pages a day', '2019-02-15 10:43:02', 30);
+INSERT INTO user_challenge (user_id, promise_id, description, start_date, days_number) VALUES (4,2,'learn how to swim', '2019-02-15 10:55:02', 20);
