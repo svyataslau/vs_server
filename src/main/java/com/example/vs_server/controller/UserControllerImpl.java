38598e42781aa4994f87aad30a2f25204d12e82f
sbely <sbely@exadel.com>
2022-08-05 02:18:31 +0300

    feature: implemented Swagger UI

--- src/main/java/com/example/vs_server/controller/UserControllerImpl.java
+++ src/main/java/com/example/vs_server/controller/UserControllerImpl.java
@@ -1,27 +1,38 @@
 package com.example.vs_server.controller;
 
-import com.example.vs_server.response.ResponseFactoryImpl;
 import com.example.vs_server.model.User;
+import com.example.vs_server.response.ResponseFactoryImpl;
 import com.example.vs_server.service.UserServiceImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
+@Api(value = "/", description = "Endpoints for user entity", produces = "application/json")
 @RestController
+@RequestMapping("")
 public class UserControllerImpl implements UserController {
 
-  private final UserServiceImpl userService;
-  private final ResponseFactoryImpl responseFactory;
+    private final UserServiceImpl userService;
+    private final ResponseFactoryImpl responseFactory;
 
-  public UserControllerImpl(UserServiceImpl userService, ResponseFactoryImpl responseFactory) {
-    this.userService = userService;
-    this.responseFactory = responseFactory;
-  }
+    public UserControllerImpl(UserServiceImpl userService, ResponseFactoryImpl responseFactory) {
+        this.userService = userService;
+        this.responseFactory = responseFactory;
+    }
 
-  @Override
-  public ResponseEntity<Object> login(@RequestBody User user) {
-      return responseFactory.generateResponse("Successfully logged in!", HttpStatus.OK, userService.login(user));
-  }
+    @ApiOperation(value = "Login to user profile", response = Object.class, tags = "loginUser", httpMethod = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Success|OK"),
+            @ApiResponse(code = 401, message = "not authorized!")})
+    @Override
+    @PostMapping("/login")
+    public ResponseEntity<Object> login(@RequestBody User user) {
+        return responseFactory.generateResponse("Successfully logged in!", HttpStatus.OK, userService.login(user));
+    }
 
 }
--- src/main/java/com/example/vs_server/model/User.java
+++ src/main/java/com/example/vs_server/model/User.java
@@ -1,57 +1,63 @@
 package com.example.vs_server.model;
 
+import io.swagger.annotations.ApiModelProperty;
+
 public class User {
 
-  private long id;
-  private String nickname;
-  private String email;
-  private String password;
+    @ApiModelProperty(notes = "Database id of the User", name = "id")
+    private long id;
+    @ApiModelProperty(notes = "Nickname of the User", name = "nickname", value = "jonny3248359253")
+    private String nickname;
+    @ApiModelProperty(notes = "Email of the User", name = "email", required = true, value = "letsgo763@gmail.com")
+    private String email;
+    @ApiModelProperty(notes = "Password of the User", name = "password", required = true, value = "somePass32")
+    private String password;
 
-  public User() {
-  }
+    public User() {
+    }
 
-  public User(String nickname, String email, String password) {
-    this.nickname = nickname;
-    this.email = email;
-    this.password = password;
-  }
+    public User(String nickname, String email, String password) {
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
 
-  public User(long id, String nickname, String email, String password) {
-    this.id = id;
-    this.nickname = nickname;
-    this.email = email;
-    this.password = password;
-  }
+    public User(long id, String nickname, String email, String password) {
+        this.id = id;
+        this.nickname = nickname;
+        this.email = email;
+        this.password = password;
+    }
 
-  public long getId() {
-    return id;
-  }
+    public long getId() {
+        return id;
+    }
 
-  public void setId(long id) {
-    this.id = id;
-  }
+    public void setId(long id) {
+        this.id = id;
+    }
 
-  public String getNickname() {
-    return nickname;
-  }
+    public String getNickname() {
+        return nickname;
+    }
 
-  public void setNickname(String nickname) {
-    this.nickname = nickname;
-  }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 
-  public String getEmail() {
-    return email;
-  }
+    public String getEmail() {
+        return email;
+    }
 
-  public void setEmail(String email) {
-    this.email = email;
-  }
+    public void setEmail(String email) {
+        this.email = email;
+    }
 
-  public String getPassword() {
-    return password;
-  }
+    public String getPassword() {
+        return password;
+    }
 
-  public void setPassword(String password) {
-    this.password = password;
-  }
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
