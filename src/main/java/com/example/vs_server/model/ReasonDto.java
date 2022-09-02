9cf235dda02d20613c207f39c84d4a32d295a115
sbely <sbely@exadel.com>
2022-09-02 08:57:39 +0300

    fix: solve NoDefault constructor error

--- src/main/java/com/example/vs_server/model/ReasonDto.java
+++ src/main/java/com/example/vs_server/model/ReasonDto.java
@@ -2,11 +2,13 @@ package com.example.vs_server.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 public class ReasonDto {
     private Long id;
     @JsonProperty("user_challenge_id")
-    private final Long userChallengeId;
-    private final String description;
+    private Long userChallengeId;
+    private String description;
 }
