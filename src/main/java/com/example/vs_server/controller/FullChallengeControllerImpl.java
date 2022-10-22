0861cf188e49a5e93a67a2aea9e66cd37a30c579
sbely <svtslv17@gmail.com>
2022-10-22 22:09:10 +0200

    refactor: delete useless responseFactory

--- src/main/java/com/example/vs_server/controller/FullChallengeControllerImpl.java
+++ src/main/java/com/example/vs_server/controller/FullChallengeControllerImpl.java
@@ -1,15 +1,14 @@
 package com.example.vs_server.controller;
 
 import com.example.vs_server.model.FullChallenge;
-import com.example.vs_server.response.ResponseFactory;
 import com.example.vs_server.service.CommonService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -17,26 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class FullChallengeControllerImpl implements FullChallengeController {
 
     private final CommonService<FullChallenge> fullChallengeService;
-    private final ResponseFactory responseFactory;
 
     @Override
-    public ResponseEntity<Object> getAllFullChallenges() {
-        return responseFactory.generateResponse("All full challenges received.", HttpStatus.OK, fullChallengeService.getAll());
+    public List<FullChallenge> getAllFullChallenges() {
+        return fullChallengeService.getAll();
     }
 
     @Override
-    public ResponseEntity<Object> save(@RequestBody FullChallenge fullChallenge) {
-        return responseFactory.generateResponse("Full challenge created.", HttpStatus.OK, fullChallengeService.create(fullChallenge));
+    public int save(@RequestBody FullChallenge fullChallenge) {
+        return fullChallengeService.create(fullChallenge);
     }
 
     @Override
-    public ResponseEntity<Object> update(@PathVariable("id") long id, @RequestBody FullChallenge fullChallenge) {
-        return responseFactory.generateResponse("Full challenge updated.", HttpStatus.OK, fullChallengeService.updateById(id, fullChallenge));
+    public FullChallenge update(@PathVariable("id") long id, @RequestBody FullChallenge fullChallenge) {
+        return fullChallengeService.updateById(id, fullChallenge);
     }
 
     @Override
-    public ResponseEntity<Object> delete(long id) {
-        return responseFactory.generateResponse("Full challenge deleted.", HttpStatus.OK, fullChallengeService.deleteById(id));
+    public int delete(long id) {
+        return fullChallengeService.deleteById(id);
     }
 
 }
--- src/main/java/com/example/vs_server/controller/PromiseControllerImpl.java
+++ src/main/java/com/example/vs_server/controller/PromiseControllerImpl.java
@@ -1,15 +1,14 @@
 package com.example.vs_server.controller;
 
 import com.example.vs_server.model.Promise;
-import com.example.vs_server.response.ResponseFactory;
 import com.example.vs_server.service.CommonService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
@@ -17,26 +16,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class PromiseControllerImpl implements PromiseController {
 
     private final CommonService<Promise> promiseService;
-    private final ResponseFactory responseFactory;
 
     @Override
-    public ResponseEntity<Object> getAllPromises() {
-        return responseFactory.generateResponse("All promises received.", HttpStatus.OK, promiseService.getAll());
+    public List<Promise> getAllPromises() {
+        return promiseService.getAll();
     }
 
     @Override
-    public ResponseEntity<Object> save(@RequestBody Promise promise) {
-        return responseFactory.generateResponse("Promise created.", HttpStatus.OK, promiseService.create(promise));
+    public int save(@RequestBody Promise promise) {
+        return promiseService.create(promise);
     }
 
     @Override
-    public ResponseEntity<Object> update(@PathVariable("id") long id, @RequestBody Promise promise) {
-        return responseFactory.generateResponse("Promise updated.", HttpStatus.OK, promiseService.updateById(id, promise));
+    public Promise update(@PathVariable("id") long id, @RequestBody Promise promise) {
+        return promiseService.updateById(id, promise);
     }
 
     @Override
-    public ResponseEntity<Object> delete(long id) {
-        return responseFactory.generateResponse("Promise deleted.", HttpStatus.OK, promiseService.deleteById(id));
+    public int delete(long id) {
+        return promiseService.deleteById(id);
     }
 }
 
